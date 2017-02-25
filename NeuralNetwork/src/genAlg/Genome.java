9911af208e4f3776b3167d105692b4d929fb0649
Owloid <lee2363@purdue.edu>
2017-02-25 10:58:00 -0600

    Add genetic algorithm framework

--- NeuralNetwork/src/genAlg/Genome.java
+++ NeuralNetwork/src/genAlg/Genome.java
@@ -1,9 +1,22 @@
 package genAlg;
 
+import fileSystem.ImageInput;
+import network.NeuralNetwork;
+
 /**
  * Contains the Genome of a NeuralNet and the fitness of the network.
  *
  */
 public class Genome
 {
+	public Genome(ImageInput input)
+	{
+		// TODO Auto-generated constructor stub
+	}
+
+	public static Genome getSeed()
+	{
+		// TODO Auto-generated method stub
+		return null;
+	}
 }
