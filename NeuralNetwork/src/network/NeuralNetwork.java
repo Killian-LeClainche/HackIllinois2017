12919850d07527ea13c25fdbe759d6bdbf6d5089
Owloid <lee2363@purdue.edu>
2017-02-25 03:34:42 -0600

    Add NeuralNetwork file

---
+++ NeuralNetwork/src/network/NeuralNetwork.java
@@ -0,0 +1,23 @@
+package network;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * TODO
+ * 
+ */
+public class NeuralNetwork
+{
+	private List<Node> inputs;
+	private List<Node> outputs;
+	
+	/**
+	 * Initialized empty values for the new NeuralNet.
+	 */
+	public NeuralNetwork()
+	{
+		inputs = new ArrayList<Node>();
+		outputs = new ArrayList<Node>();
+	}
+}
