1e650904371744b766bbefed3af87a00d901b905
Killian-LeClainche <kleclain@purdue.edu>
2017-02-25 16:13:53 -0600

    Simplification of the fitnessfinder class

--- NeuralNetwork/src/org/openimage/network/FitnessFinder.java
+++ NeuralNetwork/src/org/openimage/network/FitnessFinder.java
@@ -24,24 +24,23 @@ public class FitnessFinder
 
 	/**
 	 * 
-	 * @param trainingSet
-	 *            A list of classification training images
+	 * @param classificationNames
+	 *            The name of the classifications
+	 * @param classifications
+	 *            A list of random samplings from the pools.
 	 * @return
 	 */
-	public double find(SamplePool pool, double[][] ... classifications)
+	public double find(String[] classificationNames, double[][][] classifications)
 	{
 		double fitnessTotal = 0.0;
+		String result;
 
 		for(int i = 0; i < classifications.length; i++)
 		{
-			String name = pool.getClassificationName(i);
-			
-			String result;
-			
 			for(int j = 0; j < classifications[i].length; j++)
 			{
 				result = network.classify(classifications[i][j]);
-				if(result.equals(pool.getClassificationName(i)))
+				if(result.equals(classificationNames[i]))
 				{
 					fitnessTotal++;
 				}
