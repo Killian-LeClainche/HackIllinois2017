66c67ee4ae3b3012cf0c3cfcfa74d9a9623d334d
Owloid <lee2363@purdue.edu>
2017-02-25 12:36:59 -0600

    Add comment

--- NeuralNetwork/src/org/openimage/io/Classification.java
+++ NeuralNetwork/src/org/openimage/io/Classification.java
@@ -6,52 +6,57 @@ import java.io.IOException;
 
 import javax.imageio.ImageIO;
 
-public class Classification 
+/**
+ * A (image, classification) pair used to train the neural network or access the
+ * fitness of the neural network
+ *
+ */
+public class Classification
 {
-	
+
 	public static Classification create(File classFolder) throws IOException
 	{
 		String name = classFolder.getName();
-		
+
 		File[] subFiles = classFolder.listFiles();
 		double[][] values = new double[subFiles.length][];
 		BufferedImage image = null;
-		
-		for(int i = 0; i < subFiles.length; i++)
+
+		for (int i = 0; i < subFiles.length; i++)
 		{
 			File f = subFiles[i];
-			
+
 			image = ImageIO.read(f);
 			values[i] = new double[image.getWidth() * image.getHeight()];
-			
-			for(int j = 0; j < values[i].length; j++) 
+
+			for (int j = 0; j < values[i].length; j++)
 			{
 				long bits = image.getRGB(j / image.getWidth(), j % image.getWidth());
 				bits &= 16777215;
-				bits <<= 28; //shifting into mantissa position
-				bits |= 0x3FE0000000000000L; //setting exponent to -1
-				
+				bits <<= 28; // shifting into mantissa position
+				bits |= 0x3FE0000000000000L; // setting exponent to -1
+
 				values[i][j] = Double.longBitsToDouble(bits);
 			}
 		}
-		
+
 		return new Classification(name, values);
 	}
-	
+
 	private String classification;
 	private double[][] imageInputNodes;
-	
+
 	private Classification(String name, double[][] values)
 	{
 		classification = name;
 		imageInputNodes = values;
 	}
-	
+
 	public String getClassification()
 	{
 		return classification;
 	}
-	
+
 	public double[][] getImageInputNodes()
 	{
 		return imageInputNodes;
