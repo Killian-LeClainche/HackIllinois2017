a0c11f91d150d8e512c27a1842e1c3e374f56ebc
Killian-LeClainche <kleclain@purdue.edu>
2017-02-26 10:57:23 -0600

    Documentation for Genome

--- NeuralNetwork/src/org/openimage/genetic/Genome.java
+++ NeuralNetwork/src/org/openimage/genetic/Genome.java
@@ -5,47 +5,72 @@ import java.util.List;
 
 /**
  * Contains the Genome of a NeuralNet and the fitness of the network.
- *
  */
 public class Genome implements Comparable<Genome>
 {
+	/**
+	 * the list of the genome weights
+	 */
 	public List<Double> weights;
+
+	/**
+	 * fitness of the genome calculated in fitness finder
+	 */
 	public double fitness;
+
+	/**
+	 * Id of the genome, not entirely implemented currently.
+	 */
 	public int id;
-	
+
+	/**
+	 * Default Constructor of Genome
+	 */
 	public Genome()
 	{
 		fitness = 0;
 	}
 
+	/**
+	 * Constructor of Genome
+	 * @param list : the list of weights you want to copy over to this genome.
+	 * @param id : the id of the genome.
+	 */
 	public Genome(List<Double> list, int id)
 	{
 		this.weights = new ArrayList<Double>(list);
 		this.fitness = 0;
 		this.id = id;
 	}
-	
+
+	/**
+	 * Constructor of Genome
+	 * @param list : the list of weights you want to copy over to this genome.
+	 */
 	public Genome(List<Double> list)
 	{
 		this(list,0);
 	}
 
 	@Override
+	/**
+	 * Compare to function for genome, 
+	 * we invert the returns so sorting will provide earlier points in the array will be bigger values.
+	 */
 	public int compareTo(Genome g)
 	{
 		if (this.fitness < g.fitness)
-		{
 			return 1;
-		} else if (this.fitness > g.fitness)
-		{
+		else if (this.fitness > g.fitness)
 			return -1;
-		} else
-		{
-			return 0;
-		}
+		return 0;
 
 	}
 
+	/**
+	 * Get the weights of the genome.
+	 * @return
+	 */
 	public List<Double> getWeights()
 	{
 		return weights;
